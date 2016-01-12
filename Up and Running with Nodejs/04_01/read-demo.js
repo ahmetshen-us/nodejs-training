2a9e2e5f25ebf3ec21e8e057d69b264703c1b3e0
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-12 08:24:42 -0500

    Reading from files

--- Up and Running with Nodejs/04_01/read-demo.js
+++ Up and Running with Nodejs/04_01/read-demo.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
 var data = require('./data1.json');
-
+// Reading from files
+console.log(data)
 console.log(data.name);
 
 fs.readFile('./data1.json', 'utf-8', function(err, data) {
-    
+    console.log(data)
+    console.log(data.name)
     data = JSON.parse(data);
     console.log(data.name);
+    // console.log(data)
 });
