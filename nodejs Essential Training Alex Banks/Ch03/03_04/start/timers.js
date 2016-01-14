35090795767c91f0cee70190311c1d16635d7576
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-14 11:09:51 -0500

    setTimeout(function() { }, waitTime);

---
+++ nodejs Essential Training Alex Banks/Ch03/03_04/start/timers.js
@@ -0,0 +1,12 @@
+// Global timing functions
+var waitTime = 3000;
+var currentTime = 0;
+var waitInterval = 10;
+var percentWaited = 0;
+
+
+setTimeout(function() {
+	// clearInterval(interval);
+	// writeWaitingPercent(100);
+	console.log("\n\n done \n\n");
+}, waitTime);
