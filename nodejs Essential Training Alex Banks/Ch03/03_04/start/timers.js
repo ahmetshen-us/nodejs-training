e2cd7c9e30a9f0c234910077bf1f22e975c7f739
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-14 11:24:59 -0500

    Global timing functions

--- nodejs Essential Training Alex Banks/Ch03/03_04/start/timers.js
+++ nodejs Essential Training Alex Banks/Ch03/03_04/start/timers.js
@@ -1,12 +1,30 @@
 // Global timing functions
 var waitTime = 3000;
 var currentTime = 0;
-var waitInterval = 10;
+var waitInterval = 500;
 var percentWaited = 0;
+console.log("Wait for it");
 
+function writeWaitingPercent(p) {
+	process.stdout.clearLine();
+	process.stdout.cursorTo(0);
+	process.stdout.write(`waiting ... ${p}% `);
+}
+
+var interval = setInterval(function() {
+	currentTime += waitInterval;
+console.log(`waiting ${currentTime/1000} seconds ...`);
+
+percentWaited = Math.floor((currentTime/waitTime) * 100);
+	writeWaitingPercent(percentWaited);
+
+}, waitInterval);
 
 setTimeout(function() {
-	// clearInterval(interval);
-	// writeWaitingPercent(100);
+	clearInterval(interval);
+	writeWaitingPercent(100);
 	console.log("\n\n done \n\n");
 }, waitTime);
+
+process.stdout.write("\n\n");
+writeWaitingPercent(percentWaited);
