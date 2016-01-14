b5c09c5e3d213197795e7a9db8b608406d0eab5b
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-14 10:51:19 -0500

    process.stdout.write

--- nodejs Essential Training Alex Banks/Ch03/03_03/start/ask.js
+++ nodejs Essential Training Alex Banks/Ch03/03_03/start/ask.js
@@ -1,2 +1,34 @@
 // Standard input and standard output
-ss
+// process.stdout.write("Hello")
+// process.stdout.write("World \n\n\n\n")
+// process.stdout.write("Hello")
+
+var questions = [
+  "What is your name?",
+  "What is your fav hobby?",
+  "What is your preferred programming language?"
+];
+
+var answers = [];
+
+function ask(i) {
+  process.stdout.write(`\n\n\n\n ${questions[i]}`);
+  process.stdout.write("  >  ");
+}
+
+process.stdin.on('data', function(data) {
+process.stdout.write('\n'+data);
+process.stdout.write('\n'+data.toString());
+process.stdout.write('\n'+data.toString().trim()+'\n');
+
+	answers.push(data.toString().trim());
+  //
+	// if (answers.length < questions.length) {
+	// 	ask(answers.length);
+	// } else {
+	// 	process.exit();
+	// }
+
+});
+
+ask(0)
