1fad20591db7b0fdd5caba7f5c113b5017f8dae1
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-15 09:06:37 -0500

    Core modules

--- nodejs Essential Training Alex Banks/Ch04/04_01/start/core.js
+++ nodejs Essential Training Alex Banks/Ch04/04_01/start/core.js
@@ -1,1 +1,25 @@
 // Core modules
+ var modulepath = require('path');
+ var util = require('util');
+ var v8 = require('v8');
+
+
+console.log(modulepath);
+console.log("===================================");
+
+
+console.log(modulepath.basename);
+console.log("===================================");
+
+
+console.log(modulepath.basename(__filename));
+console.log("===================================");
+
+var dirUploads = modulepath.join(__dirname, 'www', 'files', 'uploads');
+console.log(dirUploads);
+
+
+console.log("===================================");
+util.log(dirUploads);
+
+util.log(v8.getHeapStatistics());
