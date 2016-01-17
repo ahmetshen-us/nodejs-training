f68ecab908501f2e2f052f95feca8f8853d00347
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-17 09:02:07 -0500

    Writable file streams

--- nodejs Essential Training Alex Banks/Ch05/05_08/start/promptPerson.js
+++ nodejs Essential Training Alex Banks/Ch05/05_08/start/promptPerson.js
@@ -1,3 +1,5 @@
+
+// /Writable file streams
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
 var fs = require("fs");
@@ -12,9 +14,11 @@ rl.question("What is the name of a real person? ", function(answer) {
 
 	realPerson.name = answer;
 
+//Writable stream
 	var stream = fs.createWriteStream(realPerson.name + ".md");
 
 	stream.write(`${realPerson.name}\n==================\n\n`);
+//Writable stream
 
 	rl.setPrompt(`What would ${realPerson.name} say? `);
 
@@ -23,11 +27,14 @@ rl.question("What is the name of a real person? ", function(answer) {
 	rl.on('line', function(saying) {
 
 		if (saying.toLowerCase().trim() === 'exit') {
+			
+			//Writable stream
 			stream.close();
 			rl.close();
 		} else {
-
 			realPerson.sayings.push(saying.trim());
+
+			//Writable stream
 		    stream.write(`* ${saying.trim()} \n`);
 
 			rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `);
@@ -43,8 +50,5 @@ rl.on('close', function() {
 
 	console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
 	process.exit();
-	
+
 });
-
-
-
