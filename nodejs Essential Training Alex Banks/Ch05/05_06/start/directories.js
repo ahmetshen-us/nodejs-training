7da3bc3bb9990c4f14903531451e50c4aba2f8f0
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-17 08:45:11 -0500

    Renaming and removing directories

---
+++ nodejs Essential Training Alex Banks/Ch05/05_06/start/directories.js
@@ -0,0 +1,18 @@
+// Renaming and removing directories
+var fs = require("fs");
+
+fs.readdirSync("./logs").forEach(function(fileName) {
+
+	fs.unlinkSync("./logs/" + fileName);
+
+});
+
+fs.rmdir("./logs", function(err) {
+
+	if (err) {
+		throw err;
+	}
+
+	console.log("Logs directory removed");
+
+});
