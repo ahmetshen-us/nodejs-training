e089d6525eb57a1fe6e332e57b1cebf8d9fd801c
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-17 08:31:37 -0500

    Writing and appending files

---
+++ nodejs Essential Training Alex Banks/Ch05/05_03/start/create.js
@@ -0,0 +1,23 @@
+// Writing and appending files
+var fs = require('fs')
+// var fs = require("fs");
+
+var md = `
+
+Sample Markdown Title
+=====================
+
+Sample subtitle
+----------------
+
+* point
+* point
+* point
+
+`;
+
+fs.writeFile("sample.md", md.trim(), function(err) {
+
+	console.log("File Created");
+
+});
