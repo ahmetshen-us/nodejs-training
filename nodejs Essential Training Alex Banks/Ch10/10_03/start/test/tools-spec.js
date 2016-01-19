0e11a50a18deb1b49f98215d92a54c73465f9c9a
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-19 15:35:27 -0500

    Mocking a server with Nock

--- nodejs Essential Training Alex Banks/Ch10/10_03/start/test/tools-spec.js
+++ nodejs Essential Training Alex Banks/Ch10/10_03/start/test/tools-spec.js
@@ -1,6 +1,7 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
-
+var nock = require("nock");
+// Mocking a server with Nock
 describe("Tools", function() {
 
 	describe("printName()", function() {
@@ -12,12 +13,20 @@ describe("Tools", function() {
 
 	describe("loadWiki()", function() {
 
+		before(function() {
+
+			nock("https://en.wikipedia.org")
+			    .get("/wiki/Abraham_Lincoln")
+			    .reply(200, "Mock Abraham Lincoln Page");
+
+		});
+
 		this.timeout(5000);
 
 		it("Load Abraham Lincoln's wikipedia page", function(done) {
 
 			tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function(html) {
-				expect(html).to.be.ok;
+				expect(html).to.equal("Mock Abraham Lincoln Page");
 				done();
 			});
 
@@ -26,5 +35,3 @@ describe("Tools", function() {
 	});
 
 });
-
-
